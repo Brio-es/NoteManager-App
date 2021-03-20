3d06de9576faa70e0f401aaf84170227dba39327
Cesar Briones <cesar.briones.aranda@gmail.com>
2021-03-20 18:52:59 -0500

    20/03/2021

--- Notepad App/MainWindow.xaml.cs
+++ Notepad App/MainWindow.xaml.cs
@@ -22,30 +22,24 @@ namespace Notepad_App
     {
         public string Note_Tittle;
         public string Note_Content;
+        public override string ToString()
+        {
+            return this.Note_Tittle;
+        }
 
     }
     public partial class MainWindow : Window
     {
+        string T_Tittle, T_content;
        
         List<Note> list_of_Notes = new List<Note>();
-        
-        
-        
-        
-
-
 
         public MainWindow()
         {
             InitializeComponent();
-
-            create_note("Book review", "This book is amazing");
-            create_note("Diary", "Today was a great day");
-            Console.WriteLine(list_of_Notes[0].Note_Content);
-            
-
+                       
         }
-        
+
         public void create_note(string atittle, string acontent) 
         {
             string bttitle;
@@ -55,14 +49,33 @@ namespace Notepad_App
             Notes.Note_Tittle = bttitle;
             Notes.Note_Content = acontent;
             list_of_Notes.Add(Notes);
-                
+            List_Notes_inspace.Items.Add(Notes);
         }
 
+        private void List_Notesxa_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
+        }
 
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Add");
+            create_note(Tittle_inspace.Text, Note_Content_inspace.Text);
+            
+        }
+        private void Button_Click_Delete(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Delete");
+        }
 
+        private void Tittle_inspace_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
 
-
-
+        private void Note_Content_inspace_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine(Note_Content_inspace.SelectedText);
+        }
     }
 }
