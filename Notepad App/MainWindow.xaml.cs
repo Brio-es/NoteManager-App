e6da738516688495332cd130330580a88a66b1e2
Cesar Briones <cesar.briones.aranda@gmail.com>
2021-03-21 12:00:34 -0500

    Now it is possible to change between notes

--- Notepad App/MainWindow.xaml.cs
+++ Notepad App/MainWindow.xaml.cs
@@ -30,17 +30,19 @@ namespace Notepad_App
     }
     public partial class MainWindow : Window
     {
-        string T_Tittle, T_content;
-       
+        string c_Tittle, c_content;
+        int current_index;
+
         List<Note> list_of_Notes = new List<Note>();
 
         public MainWindow()
         {
             InitializeComponent();
-                       
+            create_note("new note", "this is the content of the new note");
+
         }
 
-        public void create_note(string atittle, string acontent) 
+        public void create_note(string atittle, string acontent)
         {
             string bttitle;
             bttitle = atittle;
@@ -52,30 +54,89 @@ namespace Notepad_App
             List_Notes_inspace.Items.Add(Notes);
         }
 
-        private void List_Notesxa_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public void change_t_c_content()
         {
+            if (current_index != -1)
+            {
+                c_Tittle = list_of_Notes[current_index].Note_Tittle;
+                c_content = list_of_Notes[current_index].Note_Content;
+
+
+                Console.WriteLine(c_Tittle);
+                Console.WriteLine(c_content);
+
+
+                clearall();
+
+                Tittle_inspace.Text = c_Tittle;
+                Note_Content_inspace.Text = c_content;
+
+            }
+
+
 
         }
+
+        public void clearall()
+        {
+            Tittle_inspace.Clear();
+            Note_Content_inspace.Clear();
+        }
+
+        private void List_Notes_inspace_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine(List_Notes_inspace.SelectedIndex);
+            current_index = List_Notes_inspace.SelectedIndex;
+            change_t_c_content();
+
+        }
+
+        public void Update_Note() 
+        {
+            if (current_index != -1) 
+            {
+                list_of_Notes.RemoveAt(current_index);
+                List_Notes_inspace.Items.RemoveAt(current_index);
+                create_note(Tittle_inspace.Text, Note_Content_inspace.Text);
+
+
+            }
+                
+
+
+        }
+
 
         private void Button_Click_Add(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("Add");
             create_note(Tittle_inspace.Text, Note_Content_inspace.Text);
+            clearall();
             
         }
         private void Button_Click_Delete(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("Delete");
+            list_of_Notes.RemoveAt(current_index);
+            List_Notes_inspace.Items.RemoveAt(current_index);
+            clearall();
+
+
         }
 
-        private void Tittle_inspace_TextChanged(object sender, TextChangedEventArgs e)
+        
+        private void Save_button_Click(object sender, RoutedEventArgs e)
         {
-            
+            Update_Note();
         }
 
         private void Note_Content_inspace_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Console.WriteLine(Note_Content_inspace.SelectedText);
+               
+        }
+        private void Tittle_inspace_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
